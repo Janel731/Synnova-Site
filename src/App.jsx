e8fb8e1a9cda61072ab5a02742408a6a8fa49d2b
Janel <gratiasdeo418@gmail.com>
2026-05-17 00:00:02 -1200

    Assistant en pause

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
 
-    <AfriChatBootstrap></AfriChatBootstrap>
+    {/* <AfriChatBootstrap></AfriChatBootstrap> */}
       <header>
         <Navbar />
       </header>
